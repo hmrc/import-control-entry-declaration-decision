3762a250ed6e9a9ed5443d7af204d9f5cd548aef
christianmennerich <177663202+christianmennerich@users.noreply.github.com>
2025-03-10 12:18:37 +0000

    DL-15631 - Updates dependencies

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,21 +2,21 @@ import sbt.*
 
 object AppDependencies {
 
-  val bootstrapVersion = "9.2.0"
+  val bootstrapVersion = "9.11.0"
 
   val compile: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"                %% "bootstrap-backend-play-30" % bootstrapVersion,
     "com.github.java-json-tools" %  "json-schema-validator"     % "2.2.14",
-    "org.typelevel"              %% "cats-core"                 % "2.12.0",
-    "com.chuusai"                %% "shapeless"                 % "2.3.12"
+    "org.typelevel"              %% "cats-core"                 % "2.13.0",
+    "com.chuusai"                %% "shapeless"                 % "2.3.13"
   )
 
   val test: Seq[ModuleID] = Seq(
   "uk.gov.hmrc"                  %% "bootstrap-test-play-30" % bootstrapVersion  % Test,
-  "org.scalamock"                %% "scalamock"              % "6.0.0"           % Test,
-  "org.scalacheck"               %% "scalacheck"             % "1.18.0"          % Test,
-  "com.fasterxml.jackson.module" %% "jackson-module-scala"   % "2.17.2"          % Test,
-  "org.wiremock"                 %  "wiremock"               % "3.9.1"           % Test,
+  "org.scalamock"                %% "scalamock"              % "6.2.0"           % Test,
+  "org.scalacheck"               %% "scalacheck"             % "1.18.1"          % Test,
+  "com.fasterxml.jackson.module" %% "jackson-module-scala"   % "2.18.3"          % Test,
+  "org.wiremock"                 %  "wiremock"               % "3.12.1"           % Test,
   "org.scalatestplus"            %% "scalacheck-1-18"        % "3.2.19.0"        % Test
   )
 
